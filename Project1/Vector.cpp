7af5f3ad70ceeb1545dfef81a26c548a113e584b
Mandi_pv66 <Mandi_pv66@student.itstep.org>
2024-10-07 00:14:32 +0300

    Реализовал перегрузку операторов инкремента

--- Project1/Vector.cpp
+++ Project1/Vector.cpp
@@ -1,117 +1,134 @@
-#include "Vector.h"
-#include<iostream>
-#include<Windows.h>
-using namespace std;
+	#include "Vector.h"
+	#include<iostream>
+	#include<Windows.h>
+	using namespace std;
 
-Vector::Vector()
-{
-	arr = nullptr;
-	size = 0;
-}
-Vector::Vector(int s)
-{
-	size = s;
-	arr = new int[size];
-	for (int i = 0; i < size; i++) {
-		arr[i] = i + 1;
+	Vector::Vector()
+	{
+		arr = nullptr;
+		size = 0;
 	}
+	Vector::Vector(int s)
+	{
+		cout << "Constructor by 1 params";
+		size = s;
+		arr = new int[size];
+		for (int i = 0; i < size; i++) {
+			arr[i] = i + 1;
+		}
 
-}
-Vector::~Vector()
-{
-	cout << "Destructor\n";
-	delete[]arr;
-	size = 0;
+	}
+	Vector::~Vector()
+	{
+		cout << "Destructor\n";
+		delete[]arr;
+		size = 0;
 	
-}
-Vector::Vector(const Vector& obj)
-{
-	cout << "Copy constructor\n";
-	this->size = obj.size;
-	this->arr = new int[size];
-	for (int i = 0; i < size; i++)
+	}
+	Vector::Vector(const Vector& obj)
 	{
-		this->arr[i] = obj.arr[i];
+		cout << "Copy constructor\n";
+		this->size = obj.size;
+		this->arr = new int[size];
+		for (int i = 0; i < size; i++)
+		{
+			this->arr[i] = obj.arr[i];
+		}
 	}
-}
-void Vector::InputRand()
-{
-	for (int i = 0; i < size; i++)
+	void Vector::InputRand()
 	{
-		arr[i] = rand()%41 - 20; // [-20 +20]
+		for (int i = 0; i < size; i++)
+		{
+			arr[i] = rand()%41 - 20; // [-20 +20]
+		}
 	}
-}
-void Vector::Print()
-{
-	for (int i = 0; i < size; i++)
+	void Vector::Print()
 	{
-		cout << arr[i] << "\t";
+		for (int i = 0; i < size; i++)
+		{
+			cout << arr[i] << "\t";
+		}
+		cout << "\n---------------------------------------------------\n";
 	}
-	cout << "\n---------------------------------------------------\n";
-}
 
-void Vector::PushBack(int a)
-{
-	int* temp = new int[size + 1];  // содаем новый массив (тек размер +1)
-
-	for (int i = 0; i < size; i++)
+	void Vector::PushBack(int a)
 	{
-		temp[i] = arr[i]; // копируем текущие значени€ старого массива в новый массив
+		int* temp = new int[size + 1];  // содаем новый массив (тек размер +1)
+
+		for (int i = 0; i < size; i++)
+		{
+			temp[i] = arr[i]; // копируем текущие значени€ старого массива в новый массив
+		}
+		temp[size] = a; // инициализаци€ последнего элемента параметром метода
+
+		delete[] arr; // удаление старого массива!
+
+		arr = temp; // перенаправл€ем указатель в объекте на новый дин. массив
+		size++;
+
 	}
-	temp[size] = a; // инициализаци€ последнего элемента параметром метода
 
-	delete[] arr; // удаление старого массива!
-
-	arr = temp; // перенаправл€ем указатель в объекте на новый дин. массив
-	size++;
-
-}
-
-int Vector::PopBack()
-{
-	int* temp = new int[size - 1];
-	for (int i = 0; i < size - 1; i++)
+	int Vector::PopBack()
 	{
-		temp[i] = arr[i];
+		int* temp = new int[size - 1];
+		for (int i = 0; i < size - 1; i++)
+		{
+			temp[i] = arr[i];
+		}
+		int el = arr[size - 1]; // достаем последний элемент старого массива ƒќ удалени€
+
+		delete[] arr;
+		arr = temp;
+		size--;
+
+		return el;
 	}
-	int el = arr[size - 1]; // достаем последний элемент старого массива ƒќ удалени€
 
-	delete[] arr;
-	arr = temp;
-	size--;
-
-	return el;
-}
-
-Vector Vector::operator- (int a)
-{
-	Vector rez(size - a); // 5 - 2 = 3
-	for (int i = 0; i < rez.size; i++)
+	Vector Vector::operator- (int a)
 	{
-		rez.arr[i] = this->arr[i];
+		Vector rez(size - a); // 5 - 2 = 3
+		for (int i = 0; i < rez.size; i++)
+		{
+			rez.arr[i] = this->arr[i];
+		}
+		return rez;
 	}
-	return rez;
-}
 
-Vector& Vector::operator++()
-{
-	for (int i = 0; i < size; i++) {
-		arr[i] += 1;
+	Vector& Vector::operator++()
+	{
+		int* temp = new int[size + 1];
+		for (int i = 0; i < size; i++) {
+			temp[i] = arr[i];
+		}
+		temp[size] = 0;
+		delete[] arr;
+		arr = temp;
+		size++;
+		return *this;
 	}
-	return *this;
-}
 
-Vector& Vector::operator++(int)
-{
-	Vector temp
-}
+	Vector& Vector::operator++(int)
+	{
+		Vector temp(size);
+		int* newArr = new int[size + 1];
 
-Vector& Vector::operator--()
-{
-	return *this;
-}
+		for (int i = 0; i < size; i++) {
+			newArr[i] = arr[i];
+		}
+		newArr[size] = 0;
+		delete[] arr;
+		arr = newArr;
+		size++; 
 
-Vector& Vector::operator--(int)
-{
-	// TODO: insert return statement here
-}
+		return temp;
+	}
+
+	//Vector& Vector::operator--()
+	//{
+	//	return *this;
+	//}
+
+	//Vector& Vector::operator--(int)
+	//{
+	//	// TODO: insert return statement here
+	//}
--- Project1/main.cpp
+++ Project1/main.cpp
@@ -6,13 +6,6 @@ int main()
 {
 	srand(time(0));
 
-	Vector obj1(5); //-------------  1 2 3 4 5
-	obj1.InputRand();
-	obj1.Print();
-
-	Vector obj3 = obj1 - 2;// 1 2 3    
-	obj3.Print();
-	obj1.Print();
 
 	/*
 	* 
@@ -25,6 +18,7 @@ int main()
 	* obj6*=5; // каждый элемент умножаем на 5, возвращаем массив после изменения
 	
 	*/
+
 
 
 
